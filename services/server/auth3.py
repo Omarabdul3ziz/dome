9241846111b4d82830e02ace8f212793709cedf0
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2021-08-31 17:25:18 +0200

    flask-login make it really easy

--- services/server/auth3.py
+++ services/server/auth3.py
@@ -1,8 +1,7 @@
-from flask import Flask, json, request, jsonify, make_response, redirect, url_for
-from flask_login.utils import login_required
+from flask import Flask, request, jsonify, make_response, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import uuid
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 import jwt
 from functools import wraps
@@ -14,7 +13,7 @@ from flask_login import logout_user, LoginManager, UserMixin, login_user, login_
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./task2.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./task3.db'
 
 db = SQLAlchemy()
 db.init_app(app) # db.create_all(app=app)
@@ -27,7 +26,6 @@ GITHUB_ID = 'f999c153151ddfb524ce'
 GITHUB_SECRET = 'a2a5e3f47e20c2fd0afc3729ce732fe26dba419b'
 
 github_blueprint = make_github_blueprint(client_id=GITHUB_ID, client_secret=GITHUB_SECRET )
-
 app.register_blueprint(github_blueprint, url_prefix='/github_login')
 
 login_manager= LoginManager()
@@ -129,14 +127,32 @@ def logout(current_user):
 def index():
     return '<h1>Homa Page</h1>'
 
+# -------- auth with flask-login -------- #
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
 @app.route('/new_login')
 def new_login():
-    user = User.query.filter_by(name="Omarabdul3ziz").first()
+    auth = request.authorization
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Some auth missing!', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    if not user:
+        return make_response('User not found in database!', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    if user.password != auth.password:
+        return make_response('Wrong password', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
     login_user(user)
+
+    # Optinal for now ( make token / not )
+    token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+    user.token = token.decode('UTF-8')
+    db.session.commit()
     return jsonify(message="You are logged in!")
 
 @app.route('/new_logout')
@@ -155,11 +171,11 @@ def home():
 # -------- user api -------- #
 
 @app.route('/user', methods=['GET'])
-# @tocken_required
+@login_required
 def get_all_users():
 
-    # if not current_user.admin:
-    #     return jsonify(message="Do not have privilege")
+    if not current_user.admin:
+        return jsonify(message="Do not have privilege")
 
     users = User.query.all()
     output = []  # to convert from alchemy queries to iterable 
@@ -175,23 +191,24 @@ def get_all_users():
     return jsonify(users=output)
 
 @app.route('/user', methods=['POST'])
-# @tocken_required
+@login_required
 def create_user():
 
-    # if not current_user.admin:
-    #     return jsonify(message="Do not have privilege")
+    if not current_user.admin:
+        return jsonify(message="Do not have privilege")
 
     data = request.get_json()
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=True, token='')
+    # hashed_password = generate_password_hash(data['password'], method='sha256')
+    admin = data['admin']
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=data['password'], admin=admin, token='')
     db.session.add(new_user)
     db.session.commit()
 
     return jsonify(message="New user created!")
 
 @app.route('/user/<public_id>', methods=['GET'])
-@tocken_required
-def get_one_user(current_user, public_id):
+@login_required
+def get_one_user(public_id):
 
     if not current_user.admin:
         return jsonify(message="Do not have privilege")
@@ -211,8 +228,8 @@ def get_one_user(current_user, public_id):
     return jsonify(user=user_data)
 
 @app.route('/user/<public_id>', methods=['PUT'])
-@tocken_required
-def edit_user(current_user, public_id):
+@login_required
+def edit_user(public_id):
 
     if not current_user.admin:
         return jsonify(message="Do not have privilege")
@@ -231,8 +248,8 @@ def edit_user(current_user, public_id):
     return jsonify(message="User edited!")
 
 @app.route('/user/<public_id>', methods=['DELETE'])
-@tocken_required
-def delete_user(current_user, public_id):
+@login_required
+def delete_user(public_id):
 
     if not current_user.admin:
         return jsonify(message="Do not have privilege")
@@ -279,8 +296,8 @@ def create_task():
     return jsonify(message='Task created!')
 
 @app.route('/task/<task_id>', methods=["GET"])
-@tocken_required
-def get_one_task(current_user, task_id):
+@login_required
+def get_one_task(task_id):
     task = Todo.query.filter_by(id=task_id, user_id=current_user.id).first()
 
     if not task:
@@ -295,8 +312,8 @@ def get_one_task(current_user, task_id):
     return jsonify(task=data)
 
 @app.route('/task/<task_id>', methods=["Put"])
-@tocken_required
-def edit_task(current_user, task_id):
+@login_required
+def edit_task(task_id):
     task = Todo.query.filter_by(id=task_id, user_id=current_user.id).first()
 
     if not task:
@@ -305,15 +322,14 @@ def edit_task(current_user, task_id):
     data = request.get_json()
 
     task.text = data['text']
-    task.complete = data['complete']
 
     db.session.commit()
 
     return jsonify(message="Task edited!")
 
 @app.route('/task/<task_id>', methods=["DELETE"])
-@tocken_required
-def delete_task(current_user, task_id):
+@login_required
+def delete_task(task_id):
     task = Todo.query.filter_by(id=task_id, user_id=current_user.id).first()
 
     if not task:
@@ -325,8 +341,8 @@ def delete_task(current_user, task_id):
     return jsonify(message="Task deleted!")
 
 @app.route('/task/<task_id>/check', methods=["Put"])
-@tocken_required
-def check_task(current_user, task_id):
+@login_required
+def check_task(task_id):
     task = Todo.query.filter_by(id=task_id, user_id=current_user.id).first()
 
     if not task:
