63a4949b2bb68ab09c6fd54314e976ece9b840b6
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2021-09-11 01:48:49 +0200

    add auth views

---
+++ services/client/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="login">
+    <form>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Enter email"
+        v-model="username"
+      />
+      <input
+        type="password"
+        class="form-control"
+        placeholder="Password"
+        v-model="password"
+      />
+      <button type="submit" class="btn btn-primary" @click="submit">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+
+Vue.use(VueAxios, axios);
+export default {
+  name: "Login",
+  data() {
+    return {
+      // loggedIn: true,
+      username: "",
+      password: "",
+      baseUrl: process.env.VUE_APP_ROOT_API,
+    };
+  },
+  methods: {
+    submit() {
+      const path = this.baseUrl + "/auth/login";
+      const auth = { username: this.username, password: this.password };
+      Vue.axios.post(path, auth);
+    },
+  },
+};
+</script>
---
+++ services/client/src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <form>
+      <input type="text" class="form-control" placeholder="Enter email" />
+      <input type="password" class="form-control" placeholder="Password" />
+      <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Register",
+};
+</script>
